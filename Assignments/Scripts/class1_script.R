5c5e965648e935b03eb776a656f6e64ae9e53563
Stan Pugsley <s.pugsley@utah.edu>
2025-01-10 13:43:42 -0700

    class 1 script

--- Assignments/Scripts/class1_script.R
+++ Assignments/Scripts/class1_script.R
@@ -7,27 +7,6 @@ a + 1
 
 a + a
 
-# vectors:  1 dimensional list of items, same data type
-a <- LETTERS # Built in vector of capital letters
-
-# subset by vector position: the index of each item
-a[1:3]
-v <- c(1:3, 25:26)
-a[v]
-
-# return the index (trivial example)
-a[a=="B"]
-
-# create a vector of numbers and letters
-c(1:3, "A") # what happened?
-
-# Factors
-b <- factor(a)
-b
-
-c <- factor(c("small","medium", "large"))
-d <- factor(c("apple", "banana", "Peach"))
-
 # dataframes, 2 dimensional data structures, can combine different data types
 data(mtcars)
 
